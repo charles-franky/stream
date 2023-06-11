2ae3533e02f2b124590605ea33bfec89dd2ead7a
charles-franky <65220286+charles-franky@users.noreply.github.com>
2023-06-11 23:32:51 +0200

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,1 +1,9 @@
-
+import streamlit as st
+import mymodel as m
+st.write("""
+# Sales model
+Below are our sales predictions
+for this customer.
+""")
+window = st.slider("Forecast window(days)")
+st.write(m.run(window=window))
